a9d85719186f7b991b786899ea20535b04ccefe3
Pregum <pregum.y@gmail.com>
2024-11-24 09:48:09 +0900

    update: riverpodのproviderツリーのイベントをロギングするようにした

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:supabase_storage_rls_practice/config/my_provider_observer.dart';
 import 'package:supabase_storage_rls_practice/data/service/supabase_service.dart';
 import 'package:supabase_storage_rls_practice/routing/router.dart';
 import 'package:supabase_storage_rls_practice/routing/router.gr.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final container = ProviderContainer();
+  final container = ProviderContainer(observers: [
+    MyProviderObserver(),
+  ]);
   final client = await container.read(supabaseServiceFutureProvider.future);
   final supabaseContainer = ProviderContainer(parent: container, overrides: [
     supabaseService.overrideWithValue(client),
