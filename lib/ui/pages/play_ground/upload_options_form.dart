4b7cbfe36c6d3ec62441e0c70d534dfe8ca5ae98
Pregum <pregum.y@gmail.com>
2024-11-26 07:44:21 +0900

    update: アップロードファイルの選択UIを追加
    png以外の拡張子のファイルを追加していく

--- lib/ui/pages/play_ground/upload_options_form.dart
+++ lib/ui/pages/play_ground/upload_options_form.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:supabase_storage_rls_practice/config/logger.dart';
 import 'package:supabase_storage_rls_practice/domain/model/bucket_kind.dart';
 import 'package:supabase_storage_rls_practice/domain/model/storage_command_parameter.dart';
+import 'package:supabase_storage_rls_practice/gen/assets.gen.dart';
 
 class UploadOptionsForm extends HookConsumerWidget {
   final UploadCommandParameter parameter;
@@ -15,6 +17,14 @@ class UploadOptionsForm extends HookConsumerWidget {
     final sourceFilePathController = useTextEditingController();
     final destFilePathController = useTextEditingController();
 
+    // logger.d(Assets.images.png.values.map((e) => e.path));
+
+    useEffect(() {
+      parameter.value = parameter.value
+          .copyWith(sourceFilePath: Assets.images.png.values.first.path);
+      return () {};
+    }, []);
+
     return Form(
       child: Column(
         children: [
@@ -24,6 +34,7 @@ class UploadOptionsForm extends HookConsumerWidget {
             items: [
               for (final bucketKind in BucketKind.values)
                 DropdownMenuItem(
+                  key: ValueKey('bucket-$bucketKind'),
                   value: bucketKind,
                   child: Text(bucketKind.name),
                 ),
@@ -36,22 +47,29 @@ class UploadOptionsForm extends HookConsumerWidget {
             },
           ),
           const Text('アップロードファイルを選択してください'),
-          DropdownButton(
-            value: parameter.value.bucketKind,
+          DropdownButton<String>(
+            value: parameter.value.sourceFilePath,
             items: [
-              for (final bucketKind in BucketKind.values)
-                DropdownMenuItem(
+              for (final bucketKind
+                  in Assets.images.png.values.map((e) => e.path))
+                DropdownMenuItem<String>(
                   value: bucketKind,
-                  child: Text(bucketKind.name),
+                  child: Text(bucketKind),
                 ),
             ],
             onChanged: (newValue) {
               if (newValue == null) {
                 return;
               }
-              parameter.value = parameter.value.copyWith(bucketKind: newValue);
+              parameter.value =
+                  parameter.value.copyWith(sourceFilePath: newValue);
             },
           ),
+          if (parameter.value.sourceFilePath.isNotEmpty)
+            Image.asset(
+              parameter.value.sourceFilePath,
+              fit: BoxFit.cover,
+            ),
           const Text('操作先のパス'),
           TextField(
             controller: destFilePathController,
