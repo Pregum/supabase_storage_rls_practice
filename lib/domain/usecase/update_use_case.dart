8325df9ccf75b719df6d4bbf3f49ba9ff036cf6b
Pregum <pregum.y@gmail.com>
2024-11-29 08:02:43 +0900

    misc: 不要なimportを削除

--- lib/domain/usecase/update_use_case.dart
+++ lib/domain/usecase/update_use_case.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/services.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
