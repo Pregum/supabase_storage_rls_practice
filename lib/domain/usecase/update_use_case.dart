e43265eaeef941641630452e62eb9a56588f22cc
Pregum <pregum.y@gmail.com>
2024-11-29 07:49:16 +0900

    updaet: update用のusecaseを追加

---
+++ lib/domain/usecase/update_use_case.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_storage_rls_practice/config/logger.dart';
+import 'package:supabase_storage_rls_practice/data/repository/supabase_storage_repository.dart';
+import 'package:supabase_storage_rls_practice/domain/model/storage_command_parameter.dart';
+
+part 'update_use_case.g.dart';
+
+@Riverpod(dependencies: [SupabaseStorageRepository])
+class UpdateUseCase extends _$UpdateUseCase {
+  late final SupabaseStorageRepository _repository;
+
+  @override
+  UpdateUseCase build() {
+    final repository = ref.read(supabaseStorageRepositoryProvider);
+    _repository = repository;
+    return this;
+  }
+
+  Future<void> execute(UpdateCommandParameter parameter) async {
+    // クエリを生成する
+    final bucketName = parameter.bucketKind.name;
+    final destinationFilePath = parameter.destFilePath;
+    logger.d('parameter: $parameter');
+    final sourceFileBinary = await rootBundle.load(parameter.sourceFilePath);
+    final fileOptions = FileOptions(
+      upsert: parameter.isUpsertEnabled,
+    );
+    await _repository.updateBinary(
+      bucket: bucketName,
+      path: destinationFilePath,
+      fileBinary: sourceFileBinary.buffer.asUint8List(),
+      options: fileOptions,
+    );
+  }
+}
---
+++ lib/domain/usecase/update_use_case.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'update_use_case.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$updateUseCaseHash() => r'a0a6f39d453a8c4eec80a1a97f290e290e2916e2';
+
+/// See also [UpdateUseCase].
+@ProviderFor(UpdateUseCase)
+final updateUseCaseProvider =
+    AutoDisposeNotifierProvider<UpdateUseCase, UpdateUseCase>.internal(
+  UpdateUseCase.new,
+  name: r'updateUseCaseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$updateUseCaseHash,
+  dependencies: <ProviderOrFamily>[supabaseStorageRepositoryProvider],
+  allTransitiveDependencies: <ProviderOrFamily>{
+    supabaseStorageRepositoryProvider,
+    ...?supabaseStorageRepositoryProvider.allTransitiveDependencies
+  },
+);
+
+typedef _$UpdateUseCase = AutoDisposeNotifier<UpdateUseCase>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
