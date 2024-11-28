58a07001f9356b182e47b0257004d1e999c0e7a8
Pregum <pregum.y@gmail.com>
2024-11-29 08:03:04 +0900

    update: moveコマンド用のUseCaseを作成

---
+++ lib/domain/usecase/move_use_case.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/services.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_storage_rls_practice/config/logger.dart';
+import 'package:supabase_storage_rls_practice/data/repository/supabase_storage_repository.dart';
+import 'package:supabase_storage_rls_practice/domain/model/storage_command_parameter.dart';
+
+part 'move_use_case.g.dart';
+
+@Riverpod(dependencies: [SupabaseStorageRepository])
+class MoveUseCase extends _$MoveUseCase {
+  late final SupabaseStorageRepository _repository;
+
+  @override
+  MoveUseCase build() {
+    final repository = ref.read(supabaseStorageRepositoryProvider);
+    _repository = repository;
+    return this;
+  }
+
+  Future<void> execute(MoveCommandParameter parameter) async {
+    // クエリを生成する
+    final bucketName = parameter.bucketKind.name;
+    final oldFilePath = parameter.oldFilePath;
+    final newFilePath = parameter.newFilePath;
+    final newBucketName = parameter.newBucketKind?.name;
+    logger.d('parameter: $parameter');
+    await _repository.move(
+      bucket: bucketName,
+      oldPath: oldFilePath,
+      newPath: newFilePath,
+      destinationBucket: newBucketName,
+    );
+  }
+}
---
+++ lib/domain/usecase/move_use_case.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'move_use_case.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$moveUseCaseHash() => r'4ce73b2002e7575c46465a06ccb2277f0c96ba9c';
+
+/// See also [MoveUseCase].
+@ProviderFor(MoveUseCase)
+final moveUseCaseProvider =
+    AutoDisposeNotifierProvider<MoveUseCase, MoveUseCase>.internal(
+  MoveUseCase.new,
+  name: r'moveUseCaseProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$moveUseCaseHash,
+  dependencies: <ProviderOrFamily>[supabaseStorageRepositoryProvider],
+  allTransitiveDependencies: <ProviderOrFamily>{
+    supabaseStorageRepositoryProvider,
+    ...?supabaseStorageRepositoryProvider.allTransitiveDependencies
+  },
+);
+
+typedef _$MoveUseCase = AutoDisposeNotifier<MoveUseCase>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
