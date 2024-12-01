aea7376ad8847eb6fcb8883b9fdd46a053521f04
Pregum <pregum.y@gmail.com>
2024-12-02 08:00:38 +0900

    update: コマンド実行時にログが発行されたことがわかるようにバッジを追加

--- lib/ui/pages/setting/setting_page.dart
+++ lib/ui/pages/setting/setting_page.dart
@@ -19,39 +19,41 @@ class SettingPage extends HookConsumerWidget {
       appBar: AppBar(
         title: const Text('設定'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Center(
-            child: Column(
-              children: [
-                const Text('ユーザーid'),
-                Text(user?.id.toString() ?? ''),
-                const Text('ユーザーのメールアドレス'),
-                Text(user?.email ?? ''),
-                const Text('ユーザーのaud'),
-                Text(user?.aud ?? ''),
-                const Text('匿名ユーザー(is_anonymous)のフラグ'),
-                Text(user?.isAnonymous.toString() ?? ''),
-                const Text('プロバイダ情報'),
-                Text(
-                  user?.identities?.map((e) => e.provider).toString() ?? '',
-                  maxLines: null,
-                ),
-                const Gap(24),
-                FilledButton(
-                  child: const Text('ログアウト'),
-                  onPressed: () async {
-                    final authRepository =
-                        ref.read(supabaseAuthRepositoryProvider);
-                    await authRepository.logout();
-                    if (context.mounted) {
-                      context.router.replaceAll([const LoginRoute()]);
-                    }
-                  },
-                ),
-                const Gap(24),
-              ],
+      body: SelectionArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Center(
+              child: Column(
+                children: [
+                  const Text('ユーザーid'),
+                  Text(user?.id.toString() ?? ''),
+                  const Text('ユーザーのメールアドレス'),
+                  Text(user?.email ?? ''),
+                  const Text('ユーザーのaud'),
+                  Text(user?.aud ?? ''),
+                  const Text('匿名ユーザー(is_anonymous)のフラグ'),
+                  Text(user?.isAnonymous.toString() ?? ''),
+                  const Text('プロバイダ情報'),
+                  Text(
+                    user?.identities?.map((e) => e.provider).toString() ?? '',
+                    maxLines: null,
+                  ),
+                  const Gap(24),
+                  FilledButton(
+                    child: const Text('ログアウト'),
+                    onPressed: () async {
+                      final authRepository =
+                          ref.read(supabaseAuthRepositoryProvider);
+                      await authRepository.logout();
+                      if (context.mounted) {
+                        context.router.replaceAll([const LoginRoute()]);
+                      }
+                    },
+                  ),
+                  const Gap(24),
+                ],
+              ),
             ),
           ),
         ),
