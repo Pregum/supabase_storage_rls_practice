cd519b6a4703a8080b722ee541c8fce88c2f99d0
Pregum <pregum.y@gmail.com>
2024-12-09 07:56:29 +0900

    update: ひとまずriverpod の diのテストが動いたのでコミット

---
+++ test/domain/use_case/update_use_case_test.dart
@@ -0,0 +1,88 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:supabase_storage_rls_practice/config/logger.dart';
+import 'package:supabase_storage_rls_practice/config/my_provider_observer.dart';
+import 'package:supabase_storage_rls_practice/data/repository/supabase_storage_repository.dart';
+import 'package:supabase_storage_rls_practice/data/service/supabase_service.dart';
+import 'package:flutter/services.dart';
+
+import '../../mock/mock_obj.dart';
+import '../../utils/create_container.dart';
+
+class MockSupabaseStorageRepository extends Mock
+    implements SupabaseStorageRepository {}
+
+void main() {
+  late SupabaseStorageRepositoryMock mockRepository;
+  late ProviderContainer container;
+
+  // Uint8List のフェイク値を登録
+  setUpAll(() {
+    registerFallbackValue(Uint8List(0)); // 空の Uint8List を登録
+  });
+
+  setUp(() {
+    // Initialize the mock repository
+    mockRepository = SupabaseStorageRepositoryMock();
+
+    // Override the repository provider
+    container = createContainer(overrides: [
+      supabaseServiceProvider.overrideWithValue(MockSupabaseClient()),
+      supabaseStorageRepositoryProvider.overrideWith(() => mockRepository),
+    ]);
+  });
+
+  tearDown(() {
+    container.dispose();
+  });
+
+  test('riverpod non di test(service)', () {
+    final cont = createContainer();
+    try {
+      cont.read(supabaseServiceProvider);
+      fail('エラーが発生しない');
+    } catch (e) {
+      logger.e('error: $e');
+      expect(e, isA<UnimplementedError>());
+    }
+  });
+
+  /// サービスクラスはモックできることを確認する
+  test('riverpod test(service)', () {
+    final service = container.read(supabaseServiceProvider);
+    expect(service, isNotNull);
+    expect(service, isA<MockSupabaseClient>());
+  });
+
+  // mockしている場合はbuildを実装していないとエラーになる
+  test('riverpod test(repository)', () {
+    // ↓ がないとエラーになる
+    when(() => mockRepository.build()).thenAnswer((_) => mockRepository);
+
+    final repository = container.read(supabaseStorageRepositoryProvider);
+    expect(repository, isNotNull);
+  });
+
+  test('riverpod with mock test(repository)', () {
+    // // ↓ がないとエラーになる
+    // when(() => mockRepository.build()).thenAnswer((_) => mockRepository);
+
+    // 例外発生を確認する
+    expect(
+      () => container.read(supabaseStorageRepositoryProvider),
+      throwsA((e) => e is TypeError),
+    );
+  });
+
+  // モックでない場合はbuildメソッドが実装されているのでエラーにならないことを確認する
+  test('riverpod without mock test(repository)', () {
+    final cont = createContainer(observers: [
+      MyProviderObserver()
+    ], overrides: [
+      supabaseServiceProvider.overrideWithValue(MockSupabaseClient()),
+    ]);
+    final repository = cont.read(supabaseStorageRepositoryProvider);
+    expect(repository, isNotNull);
+  });
+}
---
+++ test/mock/mock_obj.dart
@@ -0,0 +1,17 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_storage_rls_practice/data/repository/supabase_storage_repository.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+
+class MockSupabaseClient extends Mock implements SupabaseClient {}
+
+/// [SupabaseStorageRepository] のモッククラス
+/// 継承するクラスは[_$SupabaseStorageRepository] と同じ型である必要がある
+///
+// mocktailでのモックを生成する時には、生成されたファイル内のサブクラスを指定する
+// ref: https://github.com/felangel/mocktail/issues/181#issuecomment-1407478530
+class SupabaseStorageRepositoryMock
+    extends AutoDisposeNotifier<SupabaseStorageRepository>
+    with Mock
+    implements SupabaseStorageRepository {}
